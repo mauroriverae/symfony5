40121fc9dfc1b26ecb6a136097cfd1bdf7f23a81
Mauro Rivera <mauroriverae@gmail.com>
2023-01-07 20:51:32 -0300

    Revert "adding bundle paginator and bootstrap"
    This reverts commit 879b531a462a0258270b4524c0566f2515730f2e.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,26 +8,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 // mangerregistry es el equivalente a entity manager,  puede guardar, remover, modifcar y consular la db
 use Doctrine\Persistence\ManagerRegistry;
-use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index(ManagerRegistry $doctrine, PaginatorInterface $paginator, Request $request): Response
+    public function index(ManagerRegistry $doctrine): Response
     {   
         // con solo esta linea traemos todos los registros de la db
         // findAll trae todo los post de la db
-        $query = $doctrine->getRepository(Post::class)->buscarPost();
-        $pagination = $paginator->paginate(
-            $query, /* query NOT result */
-            $request->query->getInt('page', 1), /*page number*/
-            10 /*limit per page*/
-        );
+        $posts = $doctrine->getRepository(Post::class)->findAll();
+        $post = $doctrine->getRepository(Post::class)->find(1);
         return $this->render('dashboard/index.html.twig', [
-            'pagination' => $pagination,
+            'posts' => $posts,
+            'post' => $post,
         ]);
     }
 }
